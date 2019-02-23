dc5503bff384e7606a38bb857073ee0ba26396ae
Itchicken <1398444954@qq.com>
2019-02-23 21:02:23 +0800

    添加了新文件

---
+++ HelloGit/src/cn/itchicken/domain/HelloGit.java
@@ -0,0 +1,12 @@
+package cn.itchicken.domain;
+
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello.Github");
+        System.out.println("Hello.Github");
+        System.out.println("Hello.Github");
+        System.out.println("Hello.Github");
+
+    }
+}
